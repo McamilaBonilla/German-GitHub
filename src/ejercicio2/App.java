4cff6bb94c5eb4611408cdebfc611831e69d2d49
McamilaBonilla <Mariac.bonillam@uqvirtual.edu.co>
2026-02-26 15:48:12 -0500

    Ejercicio 2: agregar linea en main

--- src/ejercicio2/App.java
+++ src/ejercicio2/App.java
@@ -3,5 +3,12 @@ package ejercicio2;
 public class App {
     public static void main(String[] args) {
         System.out.println("Ejercicio 2 - Línea principal");
+        lineaFeature();
+    }
+    public static void lineaFeature() {
+        System.out.println("Línea agregada en feature-no-conflict");
+    }
+    public static void lineaMain() {
+        System.out.println("Línea agregada en main");
     }
 }
