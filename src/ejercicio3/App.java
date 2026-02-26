2bd988fdcef4e94b76d2b2e2675522e47cbe7b87
McamilaBonilla <Mariac.bonillam@uqvirtual.edu.co>
2026-02-26 16:14:42 -0500

    Ejercicio 3: cambiar línea en master

--- src/ejercicio3/App.java
+++ src/ejercicio3/App.java
@@ -2,6 +2,18 @@ package ejercicio3;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Ejercicio 3 - Línea original");
+        // Línea modificada en master (diferente a feature-conflict)
+        System.out.println("Ejercicio 3 - Línea modificada en master");
+
+        lineaFeature();
+        lineaMain();
+    }
+
+    public static void lineaFeature() {
+        System.out.println("Línea agregada en feature-no-conflict");
+    }
+
+    public static void lineaMain() {
+        System.out.println("Línea agregada en main");
     }
 }
