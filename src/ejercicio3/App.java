353429cb26ef643cdb6b04395984a7d8197e9397
McamilaBonilla <Mariac.bonillam@uqvirtual.edu.co>
2026-02-26 16:10:39 -0500

    Ejercicio 3: cambiar línea en feature-conflict

--- src/ejercicio3/App.java
+++ src/ejercicio3/App.java
@@ -2,6 +2,14 @@ package ejercicio3;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Ejercicio 3 - Línea original");
+        System.out.println("Ejercicio 3 - Línea modificada en feature-conflict");
+        lineaFeature();
+        lineaMain();
+    }
+    public static void lineaFeature() {
+        System.out.println("Línea agregada en feature-no-conflict");
+    }
+    public static void lineaMain() {
+        System.out.println("Línea agregada en main");
     }
 }
