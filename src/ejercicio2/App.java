97c805534101cf41316617d8bb326c043f0ba98c
McamilaBonilla <Mariac.bonillam@uqvirtual.edu.co>
2026-02-26 15:57:18 -0500

    Ejercicio 2: llamar lineaMain en main

--- src/ejercicio2/App.java
+++ src/ejercicio2/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Ejercicio 2 - Línea principal");
         lineaFeature();
+        lineaMain();
     }
     public static void lineaFeature() {
         System.out.println("Línea agregada en feature-no-conflict");
