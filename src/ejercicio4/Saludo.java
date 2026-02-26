6bce698fde859f27e8cd7bc809e9c57181c2a0a2
McamilaBonilla <Mariac.bonillam@uqvirtual.edu.co>
2026-02-26 15:30:57 -0500

    agregado método en feature-ff

--- src/ejercicio4/Saludo.java
+++ src/ejercicio4/Saludo.java
@@ -4,4 +4,7 @@ public class Saludo {
     public static void mensaje() {
         System.out.println("Mensaje original en Saludo");
     }
+    public static void nuevoMetodo() {
+        System.out.println("Método agregado en feature-ff");
+    }
 }
