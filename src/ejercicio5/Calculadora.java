b2e072965ba86533a6d1adf7cb730cc752728a93
McamilaBonilla <Mariac.bonillam@uqvirtual.edu.co>
2026-02-26 17:11:44 -0500

    Agregar método multiplicar y mostrar resultado en App

--- src/ejercicio5/Calculadora.java
+++ src/ejercicio5/Calculadora.java
@@ -1,4 +1,17 @@
 package ejercicio5;
 
 public class Calculadora {
+
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+
+    public static int restar(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplicar(int a, int b) {
+        return a * b;
+    }
+
 }
