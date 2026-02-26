37a9d3cb236c0850f4a87e1bc5f481287843f700
McamilaBonilla <Mariac.bonillam@uqvirtual.edu.co>
2026-02-26 17:09:24 -0500

    Agregar método restar y mostrar resultado en App

--- src/ejercicio5/App.java
+++ src/ejercicio5/App.java
@@ -1,4 +1,12 @@
 package ejercicio5;
 
 public class App {
+    public static void main(String[] args) {
+        System.out.println("Ejercicio 5 - Calculador básico");
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Suma: " + Calculadora.sumar(a, b));
+        System.out.println("Resta: " + Calculadora.restar(a, b));
+    }
 }
--- src/ejercicio5/Calculadora.java
+++ src/ejercicio5/Calculadora.java
@@ -1,4 +1,11 @@
 package ejercicio5;
 
 public class Calculadora {
+
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+    public static int restar(int a, int b) {
+        return a - b;
+    }
 }
