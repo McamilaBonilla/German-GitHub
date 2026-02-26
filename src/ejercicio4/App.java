b190392b08bb5cfdd9e39fcadac2e909c2b89041
McamilaBonilla <Mariac.bonillam@uqvirtual.edu.co>
2026-02-26 16:32:51 -0500

    Ejercicio 4: cambios en master

--- src/ejercicio4/App.java
+++ src/ejercicio4/App.java
@@ -2,6 +2,8 @@ package ejercicio4;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Ejercicio 4 - Línea original App");
+        System.out.println("Ejercicio 4 - Línea modificada en master");
+
+        Saludo.saludar();
     }
 }
